a3712f9e6815c23ecb66d6b9348c0a3358e23e4b
Guilherme Oliveira <101231774+AlteringTriangle@users.noreply.github.com>
2022-07-19 19:55:48 -0300

    funcitions-to-class

--- Merge/functions.py
+++ Merge/functions.py
@@ -1,98 +1,189 @@
 import pandas as pd
-import numpy as  np
-
-pi = np.pi
-# from step_1... None
-# from step_2
-
-def mult_row_by(o_data, multiply_values):
-    """
-        :param o_data: dados originalis
-        :param multiply_values: valores que multiplicaram cada linha
-        dos dados originais
-        :return: tabela de dados onde cada cada valor de multiply_values
-        multiplica uma linha dos dados originais
-    """
-    n_data = o_data
-    for row in o_data.index:
-        n_data.iloc[row, 1:] = multiply_values.loc[row] * o_data.iloc[row,1:]
-    return n_data
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-horario = pd.read_excel('../Dados Modificados/Tabela de dados de horarios e potencias.xlsx', sheet_name='Planilha1')
-equipamentos = pd.read_excel('../Dados Modificados/Tabela de dados de horarios e potencias.xlsx', sheet_name='Planilha2')
-motores = pd.read_excel('../Dados Modificados/Tabela de dados de horarios e potencias.xlsx', sheet_name='Planilha3')
+class CurvaDCarga:
+    def __init__(self):
+        self.horario = pd.read_excel('../Dados Modificados/Tabela de dados de horarios e potencias.xlsx',
+                                     sheet_name='Planilha1')
+        self.equipamentos = pd.read_excel('../Dados Modificados/Tabela de dados de horarios e potencias.xlsx',
+                                          sheet_name='Planilha2')
+        self.motores = pd.read_excel('../Dados Modificados/Tabela de dados de horarios e potencias.xlsx',
+                                     sheet_name='Planilha3')
 
-cv2kw = .735499
+        self.cv2kw = .735499
+        self.cd, self.t1, self.t2, self.cd_max, self.t1_max, self.t2_max = [None, ] * 6
+        self.calc_potencias()
 
-e_n = equipamentos.loc[:, 'η (%)']
-e_fp = equipamentos.loc[:, 'fp']
-e_kVA = equipamentos.loc[:, 'kVA']
+    def mult_row_by(self, multiply_values):
+        """
+            :param o_data: dados originais
+            :param multiply_values: valores que multiplicaram cada linha
+            dos dados originais
+            :return: tabela de dados onde cada cada valor de multiply_values
+            multiplica uma linha dos dados originais
+        """
+        n_data = self.horario[:]
+        for row in n_data.index:
+            n_data.iloc[row, 1:] = multiply_values.loc[row] * n_data.iloc[row, 1:]
+        return n_data
 
-p_equipamentos = e_kVA * e_fp * e_n
-s_equipametnos = e_kVA * e_n
+    def calc_potencias(self):
+        e_n = self.equipamentos.loc[:, 'η (%)']
+        e_fp = self.equipamentos.loc[:, 'fp']
+        e_kVA = self.equipamentos.loc[:, 'kVA']
 
-m_cv = motores.loc[:, 'cv']
-m_n = motores.loc[:, 'η (%)']
-m_fp = motores.loc[:, 'fp']
+        p_equipamentos = e_kVA * e_fp * e_n
+        s_equipametnos = e_kVA * e_n
 
-p_motor = (m_cv * cv2kw * m_fp) / m_n
-s_motor = (m_cv * cv2kw) / m_n
+        m_cv = self.motores.loc[:, 'cv']
+        m_n = self.motores.loc[:, 'η (%)']
+        m_fp = self.motores.loc[:, 'fp']
 
-potencia_p_diaria = pd.concat([p_equipamentos, p_motor]).reset_index(drop=True)
-potencia_s_diaria = pd.concat([s_equipametnos, s_motor]).reset_index(drop=True)
+        p_motor = (m_cv * self.cv2kw * m_fp) / m_n
+        s_motor = (m_cv * self.cv2kw) / m_n
 
-# tabela de consumo diario
-consumo_diario = mult_row_by(horario[:], potencia_p_diaria)
+        potencia_p_diaria = pd.concat([p_equipamentos, p_motor]).reset_index(drop=True)
+        potencia_s_diaria = pd.concat([s_equipametnos, s_motor]).reset_index(drop=True)
 
-# para os transformadores
-transformador_1 = mult_row_by(horario[:], potencia_s_diaria[:])[:9]
-transformador_2 = mult_row_by(horario[:], potencia_s_diaria[:])[9:]
+        # tabela de consumo diario (P)
+        print(potencia_p_diaria)
+        consumo_diario = self.mult_row_by(potencia_p_diaria)
 
-cd_max = consumo_diario.max()
-t1_max = transformador_1.max()
-t2_max = transformador_2.max()
+        # para os transformadores (S)
+        transformador_1 = self.mult_row_by(potencia_s_diaria[:])[:9]
+        transformador_2 = self.mult_row_by(potencia_s_diaria[:])[9:]
 
-# usado para o plot
-cd = consumo_diario[:].iloc[:, 1:].sum()
-t1 = transformador_1[:].iloc[:, 1:].sum()
-t2 = transformador_2[:].iloc[:, 1:].sum()
+        self.cd_max = consumo_diario.max()
+        self.t1_max = transformador_1.max()
+        self.t2_max = transformador_2.max()
 
-# para visualizar as variaveis importantes, como elas se organizam e se estruturam
-def s1_variables():
-    print(f"""\033[33mHorario original\033[m
-{horario.to_string()}\n
-\033[33mEquipamentos\033[m
-{equipamentos.to_string()}\n
-\033[33mMotores\033[m
-{motores.to_string()}\n
-\033[33mPotência (P) dos equipamentos\033[m
-{p_equipamentos.to_string()}\n
-\033[33mPotência (S) dos equipamentos\033[m
-{s_equipametnos.to_string()}\n
-\033[33mPotência (P) dos motores\033[m
-{p_motor.to_string()}\n
-\033[33mPotência (S) dos motores\033[m
-{s_motor.to_string()}\n
-\033[33mPotência (P) dirária completa\033[m
-{potencia_p_diaria.to_string()}
-\033[33mPotência (S) dirária completa\033[m
-{potencia_s_diaria.to_string()}
-\033[33mPotência dirária consumida\033[m
-{consumo_diario.to_string()}
-\033[33mPotência dos tranformadores\033[m
-\033[32mTransformador 1\033[m
-{transformador_1}
-\033[32mTransformador 2\033[m
-{transformador_2}
-\033[34mCd\033[m
-{cd}
-\033[34mT1\033[m
-{t1}
-\033[34mT2\033[m
-{t2}
-""")
+        # usado para o plot
+        self.cd = consumo_diario[:].iloc[:, 1:].sum()
+        self.t1 = transformador_1[:].iloc[:, 1:].sum()
+        self.t2 = transformador_2[:].iloc[:, 1:].sum()
 
-# from step3()
+        # para visualizar as variaveis importantes, como elas se organizam e se estruturam
+
+    def plot_data(self):
+        fig1, ax1 = plt.subplots()
+        xtick = pd.date_range(str(self.cd.index[0]), str(self.cd.index[-1]), freq="2H").strftime('%H:%M')
+
+        ax1.plot(self.cd[:])
+        ax1.set_xticks(xtick)
+        ax1.legend('Curva de carga')
+        plt.xlabel('Horário')
+        plt.ylabel('Potência kW')
+        ax1.grid()
+
+        fig2, ax2 = plt.subplots()
+
+        ax2.plot(self.t1[:])
+        ax2.plot(self.t2[:])
+        ax2.set_xticks(xtick)
+        ax2.legend('Dimensionamento dos Transformadores')
+        plt.xlabel('Horário')
+        plt.ylabel('Potência kVA')
+        ax2.grid()
+        plt.show()
+
+    def change_horario(self, mode='rd'):
+        """
+        :param mode: rd -> horário aleatório
+        :return: modificia diretamente self.horario
+        """
+        if mode.lower() == 'rd':
+            choice = self.ask_device()
+            time_on = self.use_data(self.random_data())
+            self.change_timing(choice, time_on)
+
+    def ask_data(self):
+        """
+        O usuario digita pelo prompt a maneira que o horário
+        está organizado
+        :return: dicionario pronto para ser transformado em df/s
+        """
+        d = dict()
+        for c in range(0, 24):
+            if c <= 9:
+                c = f'0{c}:00'
+            else:
+                c = f'{c}:00'
+
+            n = input(f'hora {c}')
+            if int(n[:2]) > 0:
+                n = 1
+            else:
+                n = 0
+            d[c] = n
+        return d
+
+    def use_data(self, data):
+        """
+        :param data: parametro de dados já organizados afim de virar
+        um horario
+        :return: dicionario pronto para ser transformado em df/s
+        """
+
+        d = dict()
+        for c in range(0, 24):
+            ind = c
+            if c <= 9:
+                c = f'0{c}:00'
+            else:
+                c = f'{c}:00'
+
+            d[c] = data[ind]
+
+        return d
+
+    def random_data(self, size=24):
+        """
+        :param size: numero de divisões do horario
+        :return: uma lista aleatoria, de zeros e uns para representar o horario
+        """
+        rd = np.random.randint(0, 2, size)
+        return rd
+
+    def ask_device(self):
+
+        devices = list(self.horario.iloc[:, 0])
+        print(f'Available devices are:')
+        for i, device in enumerate(devices[:-1]):
+            if (i + 1) % 8 == 0:
+                print(f'{device}', end=',\n')
+            else:
+                print(f'{device}', end=', ')
+        print(f'{devices[-1]}.')
+        choice = input('choice one to modify (name or position number)')
+        if choice.isdigit():
+            choice = self.horario.iloc[int(choice) - 1, 0]
+        else:
+            choice = choice.upper()
+            if choice not in devices:
+                choice = None
+        if choice:
+            print(f'you choice was {choice}')
+        else:
+            print(f'your choice isn\'t in avaliable devices')
+        return choice
+
+    def change_timing(self, choice, newtime):
+        """
+        :param choice: equipamento a ser modificado
+        :param newtime: novo horario para o equipamento
+        :return: self.horario é atualizado
+        """
+        print('\033[33mold data\033[m'.capitalize())
+        print(self.horario)
+        line = self.horario.loc[self.horario.iloc[:, 0] == choice]
+        newtime['Equipamento'] = choice
+        line = pd.DataFrame(newtime, index=line.index)
+        self.horario.loc[line.index] = line
+        print('\033[33mmodified data\033[m'.capitalize())
+        print(self.horario)
 
 
+cdg = CurvaDCarga()
+cdg.plot_data()
